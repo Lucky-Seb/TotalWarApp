0668e75267a2140504f58b6245d6b93580bfda8c
Sebsatian <Sebastian@jursrasmussen.dk>
2024-02-22 13:03:38 +0100

    Add my branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,5 +37,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.android.volley:volley:1.2.1")     // adding volley dependency
-
+    implementation("com.google.code.gson:gson:2.8.9")  // Replace with the latest version if needed
 }
--- app/src/main/java/Projekt/TotalWar/App/MainActivity.java
+++ app/src/main/java/Projekt/TotalWar/App/MainActivity.java
@@ -6,6 +6,18 @@ import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,82 +31,123 @@ public class MainActivity extends AppCompatActivity {
 
         apiCallHelper = new ApiCallHelper(this);
 
-        // Example usage for a GET request
-        String apiUrlGet = "http://10.131.213.178:8080/factions";
-        apiCallHelper.makeGetRequest(apiUrlGet, new ApiCallHelper.ApiCallback() {
+        // Set up buttons for each API call
+        Button getButton = findViewById(R.id.getButton);
+        Button postButton = findViewById(R.id.postButton);
+        Button putButton = findViewById(R.id.putButton);
+        Button deleteButton = findViewById(R.id.deleteButton);
+
+        // Set up EditText fields for input
+        EditText factionNameEditText = findViewById(R.id.factionNameEditText);
+        EditText updatedFactionNameEditText = findViewById(R.id.updatedFactionNameEditText);
+
+        // Set up click listeners for each button
+        getButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onApiCompleted(String result) {
-                // Handle the API response here
-                Log.d(TAG, "GET Request Response: " + result);
-            }
+            public void onClick(View view) {
+                // Execute GET request when the GET button is clicked
+                String apiUrlGet = "http://10.131.213.178:8080/factions";
+                apiCallHelper.makeGetRequest(apiUrlGet, new ApiCallHelper.ApiCallback() {
+                    @Override
+                    public void onApiCompleted(String result) {
+                        // Handle the GET API response here
+                        Log.d(TAG, "GET Request Response: " + result);
 
-            @Override
-            public void onApiError(String error) {
-                // Handle API error
-                Log.e(TAG, "GET Request Error: " + error);
-            }
-        });
+                        // Parse JSON using Gson
+                        Gson gson = new Gson();
+                        YourDataClass data = gson.fromJson(result, YourDataClass.class);
 
-        // Example usage for a POST request
-        String apiUrlPost = "http://10.131.213.178:8080/factions/";
-        JSONObject postRequestBody = new JSONObject();
-        try {
-            postRequestBody.put("factionName", "Empire"); // Replace "Empire" with the actual value of factionName
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+                        // Now you can use 'data' object as needed
+                        Log.d(TAG, "Parsed Data: " + data.toString());
+                    }
 
-        apiCallHelper.makePostRequest(apiUrlPost, postRequestBody, new ApiCallHelper.ApiCallback() {
-            @Override
-            public void onApiCompleted(String result) {
-                // Handle the API response here
-                Log.d(TAG, "POST Request Response: " + result);
-            }
-
-            @Override
-            public void onApiError(String error) {
-                // Handle API error
-                Log.e(TAG, "POST Request Error: " + error);
-            }
-        });
-
-        // Example usage for a PUT request
-        String apiUrlPut = "http://10.131.213.178:8080/factions/{factionId}";
-        JSONObject putRequestBody = new JSONObject();
-        try {
-            putRequestBody.put("factionName", "UpdatedEmpire"); // Replace "UpdatedEmpire" with the new value of factionName
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        apiCallHelper.makePutRequest(apiUrlPut, putRequestBody, new ApiCallHelper.ApiCallback() {
-            @Override
-            public void onApiCompleted(String result) {
-                // Handle the API response here
-                Log.d(TAG, "PUT Request Response: " + result);
-            }
-
-            @Override
-            public void onApiError(String error) {
-                // Handle API error
-                Log.e(TAG, "PUT Request Error: " + error);
+                    @Override
+                    public void onApiError(String error) {
+                        // Handle GET API error
+                        Log.e(TAG, "GET Request Error: " + error);
+                    }
+                });
             }
         });
 
-        // Example usage for a DELETE request
-        String apiUrlDelete = "http://10.131.213.178:8080/factions/{factionId}";
 
-        apiCallHelper.makeDeleteRequest(apiUrlDelete, new ApiCallHelper.ApiCallback() {
+        postButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onApiCompleted(String result) {
-                // Handle the API response here
-                Log.d(TAG, "DELETE Request Response: " + result);
+            public void onClick(View view) {
+                // Execute POST request when the POST button is clicked
+                String apiUrlPost = "http://10.131.213.178:8080/factions/";
+                JSONObject postRequestBody = new JSONObject();
+                try {
+                    String factionName = factionNameEditText.getText().toString();
+                    postRequestBody.put("factionName", factionName);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                apiCallHelper.makePostRequest(apiUrlPost, postRequestBody, new ApiCallHelper.ApiCallback() {
+                    @Override
+                    public void onApiCompleted(String result) {
+                        // Handle the POST API response here
+                        Log.d(TAG, "POST Request Response: " + result);
+                    }
+
+                    @Override
+                    public void onApiError(String error) {
+                        // Handle POST API error
+                        Log.e(TAG, "POST Request Error: " + error);
+                    }
+                });
             }
+        });
 
+        putButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onApiError(String error) {
-                // Handle API error
-                Log.e(TAG, "DELETE Request Error: " + error);
+            public void onClick(View view) {
+                // Execute PUT request when the PUT button is clicked
+                String apiUrlPut = "http://10.131.213.178:8080/factions/{factionId}";
+                JSONObject putRequestBody = new JSONObject();
+                try {
+                    String updatedFactionName = updatedFactionNameEditText.getText().toString();
+                    putRequestBody.put("factionName", updatedFactionName);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                apiCallHelper.makePutRequest(apiUrlPut, putRequestBody, new ApiCallHelper.ApiCallback() {
+                    @Override
+                    public void onApiCompleted(String result) {
+                        // Handle the PUT API response here
+                        Log.d(TAG, "PUT Request Response: " + result);
+                    }
+
+                    @Override
+                    public void onApiError(String error) {
+                        // Handle PUT API error
+                        Log.e(TAG, "PUT Request Error: " + error);
+                    }
+                });
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Execute DELETE request when the DELETE button is clicked
+                String apiUrlDelete = "http://10.131.213.178:8080/factions/{factionId}";
+
+                apiCallHelper.makeDeleteRequest(apiUrlDelete, new ApiCallHelper.ApiCallback() {
+                    @Override
+                    public void onApiCompleted(String result) {
+                        // Handle the DELETE API response here
+                        Log.d(TAG, "DELETE Request Response: " + result);
+                    }
+
+                    @Override
+                    public void onApiError(String error) {
+                        // Handle DELETE API error
+                        Log.e(TAG, "DELETE Request Error: " + error);
+                    }
+                });
             }
         });
     }
